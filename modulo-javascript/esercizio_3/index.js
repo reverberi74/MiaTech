a1bd3493e704bd2036cfb9ab3f384bd8ce095f66
reverberi74 <luca.calvo44@gmail.com>
2025-03-03 16:08:48 +0100

    Ridefinizione di variabili

---
+++ modulo-javascript/esercizio_3/index.js
@@ -0,0 +1,7 @@
+// Ridefinizione di variabili
+let number = 15;
+{
+    let number = 30;
+    console.log("La variabile dentro il blocco vale "+number);
+}
+console.log("La variabile fuori il blocco vale "+number);
