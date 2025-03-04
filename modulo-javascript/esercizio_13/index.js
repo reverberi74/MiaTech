3655a77ad5f73269c245db222b6147e136016b77
reverberi74 <luca.calvo44@gmail.com>
2025-03-04 11:06:27 +0100

    Array di numeri

---
+++ modulo-javascript/esercizio_13/index.js
@@ -0,0 +1,5 @@
+// Array di numeri
+let numeri = [11, 22, 33, 44, 55]; // Creazione dell'array
+console.log("L'array è: "+numeri); // Stampa l'array in console
+console.log("Gli elementi dell'array sono: ");
+numeri.forEach(numero => console.log(numero)); // Stampa gli elementi uno ad uno
