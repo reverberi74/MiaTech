33a38d84bb239e63c74e2a23f4d23675caba7779
reverberi74 <luca.calvo44@gmail.com>
2025-03-18 09:59:36 +0100

    Estrazione elementi base

---
+++ modulo-javascript/esercizio_47/index.js
@@ -0,0 +1,10 @@
+// Estrazione elementi base
+// Dichiarazione di un array di numeri
+const numeri = [10, 20, 30, 40, 50];
+
+// Destructuring per assegnare i primi due elementi a due variabili
+const [primo, secondo] = numeri;
+
+// Stampa delle variabili nella console
+console.log("Primo numero:", primo);  // Output: Primo numero: 10
+console.log("Secondo numero:", secondo);  // Output: Secondo numero: 20
