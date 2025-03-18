5f7e78ce051653361e57dab783a2654537ec2899
reverberi74 <luca.calvo44@gmail.com>
2025-03-18 10:35:29 +0100

    Destructuring con valori predefiniti

--- modulo-javascript/esercizio_49/index.js
+++ modulo-javascript/esercizio_49/index.js
@@ -6,6 +6,6 @@ const [primo, secondo, terzo, quarto = 56, quinto = 78 ] = numeri;
 
 console.log("Primo numero:", primo);  // Output: Primo numero: 15
 console.log("Secondo numero:", secondo);  // Output: Secondo numero: 28
-console.log("Terzo numero:", terzo);  // Output: Primo numero: 35
-console.log("Quarto numero:", quarto);  // Output: Secondo numero: 56
-console.log("Quinto numero:", quinto);  // Output: Secondo numero: 78
+console.log("Terzo numero:", terzo);  // Output: Terzo numero: 35
+console.log("Quarto numero:", quarto);  // Output: Quarto numero: 56
+console.log("Quinto numero:", quinto);  // Output: Quinto numero: 78
