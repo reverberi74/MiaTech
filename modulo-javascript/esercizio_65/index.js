e17fc15f3d943612f47f2e787ad11ecbbda3981b
reverberi74 <luca.calvo44@gmail.com>
2025-03-20 10:32:52 +0100

    Utilizzare console.log

---
+++ modulo-javascript/esercizio_65/index.js
@@ -0,0 +1,17 @@
+// Utilizzare console.log
+// Dichiarazione di variabili di diversi tipi di dati
+let numero = 25; // Numero
+let testo = "Hello, world!"; // Stringa
+let lista = ["Mela", "Banana", "Arancia"]; // Array
+let persona = { nome: "Luca", cognome: "Calvo", eta: 50 }; // Oggetto
+
+// Stampa dei valori nella console
+console.log("Numero:", numero);
+console.log("Stringa:", testo);
+console.log("Array:", lista);
+console.log("Oggetto:", persona);
+
+// Stampa dettagliata dell'oggetto
+console.log("Nome:", persona.nome);
+console.log("Cognome:", persona.cognome);
+console.log("Età:", persona.eta);
