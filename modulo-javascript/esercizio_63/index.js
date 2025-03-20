212b3985103e123a3751f62e3a9ade7f7eac9260
reverberi74 <luca.calvo44@gmail.com>
2025-03-20 09:01:05 +0100

    Stringa multilinea con template literals

---
+++ modulo-javascript/esercizio_63/index.js
@@ -0,0 +1,21 @@
+// Stringa multilinea con template literals
+const nome = "Luca";
+const cognome = "Calvo";
+const eta = 50;
+const citta = "Siracusa";
+
+// Creazione della stringa multilinea utilizzando i template literals
+const infoPersona = `
+Nome: ${nome}
+Cognome: ${cognome}
+Età: ${eta} anni
+Città: ${citta}
+`;
+
+// Stampa della stringa risultante
+console.log(infoPersona);
+/*output:
+Nome: Luca
+Cognome: Calvo
+Età: 50 anni
+Città: Siracusa*/
