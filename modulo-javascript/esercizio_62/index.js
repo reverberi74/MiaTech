bc80139176072df28a304043ccd076517e7121bf
reverberi74 <luca.calvo44@gmail.com>
2025-03-20 08:51:33 +0100

    Stringa semplice con Template Literals

---
+++ modulo-javascript/esercizio_62/index.js
@@ -0,0 +1,9 @@
+// Stringa semplice con Template Literals
+const nome = "Luca";
+const cognome = "Calvo";
+
+// Creazione della stringa utilizzando i template literals
+const nomeCompleto = `Il nome completo è: ${nome} ${cognome}`;
+
+// Stampa della stringa risultante
+console.log(nomeCompleto); // output: Il nome completo è: Luca Calvo
